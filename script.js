383072fc270e547f59f24044eec50c7ad530bc49
badrinath-pillalamarri <badrinathpillalamarri@gmail.com>
2024-11-10 19:47:43 +0530

    skills orbit added

--- script.js
+++ script.js
@@ -8,3 +8,56 @@ let timer = setInterval(()=> {
     nameHeader.innerHTML = currentLoadedName
     i++;
 },100)
+
+document.addEventListener('DOMContentLoaded', () => {
+//   const skills = [
+//       'code-logos/html.png',
+//       'code-logos/js.png',
+//       'code-logos/css.png',
+//       'code-logos/react.png',
+//       'code-logos/mongoDB.png',
+//       'code-logos/nodejs.png',
+//       'code-logos/expressjs.png',
+//       'code-logos/c.png',
+//       'code-logos/cpp.png',
+//       'code-logos/tailwind.png'
+//   ];
+  const skills = [
+      'code-logos/html.png',
+      'code-logos/cpp.svg',
+      'code-logos/js.svg',
+      'code-logos/css.png',
+      'code-logos/react.svg',
+      'code-logos/mongodb.svg',
+      'code-logos/nodejs.svg',
+      'code-logos/express.svg',
+      'code-logos/c.png',
+      'code-logos/tailwind.svg',
+  ];
+
+  const orbitContainer = document.getElementById('skills-orbit');
+  skills.forEach((skill, index) => {
+      const skillWrapper = document.createElement('div');
+      skillWrapper.style.transform = `rotate(${index * (360 / skills.length)}deg)`;
+      
+      const skillDiv = document.createElement('div');
+      skillDiv.className = 'skill';
+      skillDiv.style.right = `${(index %2) ? '7.5em' : '3.5em'}`;
+      
+
+      const img = document.createElement('img');
+      img.className = 'logo';
+      img.src = skill;
+      img.alt = 'Logo';
+      img.style.transform = `rotate(${index * -1 * (360 / skills.length)}deg)`;
+
+      skillDiv.appendChild(img);
+      skillWrapper.appendChild(skillDiv);
+      orbitContainer.appendChild(skillWrapper);
+  });
+});
+
+
+// <div className="skill" transform="rotate(${index * (360 / skills.length)}deg)"  right=`${index % 2 ? '7.5em' : '3.5em'}`>
+//     <img src={img-skill} transform = `rotate(${index * -1 * (360 / skills.length)}deg)`
+// </div>
